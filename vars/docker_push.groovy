d8c0d41baf648efd75ebcff182d90f92f104b422
mysahgithub <86221498+mysahgithub@users.noreply.github.com>
2026-06-08 21:09:01 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String Project, String ImageTag){
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred',
+                                    passwordVariable: 'dockerHubPass',
+                                    usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+      sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
